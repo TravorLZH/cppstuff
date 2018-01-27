9f18b42a20660b2271903fc1f328b4e4c672262c
Travor Liu <travor_lzh@outlook.com>
2018-01-27 18:33:21 -0500

    Added Demonstration of vector and iterator

--- vectordemo.cpp
+++ vectordemo.cpp
@@ -5,4 +5,18 @@
 #include <iostream>
 #include <vector>
 
-using namespace std
+using namespace std;
+
+int main(void){
+    vector<string> v;
+    v.push_back("Hello");
+    v.push_back("World");
+    v.push_back("Jason");
+    v.push_back("Travor");
+    v.push_back("Percy");
+    vector<string>::iterator it=v.begin();
+    for(;it!=v.end();it++){
+        cout << *it << endl;
+    }
+    return 0;
+}
