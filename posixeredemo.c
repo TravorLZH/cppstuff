1db4af7c03c5c11391677770f785eb13d0a1516e
Travor Liu <travor_lzh@outlook.com>
2018-02-25 13:20:09 -0500

    New program

---
+++ posixeredemo.c
@@ -0,0 +1,16 @@
+#include <regex.h>
+#include <stdio.h>
+
+int main(void){
+	regex_t re;
+	regmatch_t matches[10];
+	char str[]="Travor Liu travor_lzh@outlook.com 15011964779";
+	regcomp(&re,"1[0-9]{10}",REG_EXTENDED);
+	printf("Demonstration of POSIX ERE");
+	printf("Now matching chinese phone number from the following string:\n%s\n",str);
+	regexec(&re,str,10,matches,NULL);
+	str[matches[0].rm_eo]='\0';
+	printf("Matched result: %s\n",str+matches[0].rm_so);
+	return 0;
+}
+
