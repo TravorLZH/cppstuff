56c124638828f6f9bd800d229efe13274a0ef0a7
Travor Liu <travor_lzh@outlook.com>
2018-03-21 19:50:06 -0400

    Added bitfield demo

---
+++ bitfield.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct{
+	char a:1;
+	char b:1;
+	char c:4;
+	char d:2;
+} bitfield;
+
+int main(int argc,char **argv){
+	bitfield.a=1;
+	bitfield.b=0;
+	bitfield.c=0b1001;	// GCC Extension
+	bitfield.d=0;
+	char x=*(char*)&bitfield;
+	printf("bitfield: 0x%x\n",x);
+	return 0;
+}
